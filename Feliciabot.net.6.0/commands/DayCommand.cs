eb52e426cbb8f5a313fe1dae9575e3c39a22b800
hamdong <mrzombielover@gmail.com>
2023-08-19 10:37:40 -0400

    Refactored Daycommand

--- Feliciabot.net.6.0/commands/DayCommand.cs
+++ Feliciabot.net.6.0/commands/DayCommand.cs
@@ -7,10 +7,20 @@ namespace Feliciabot.net._6._0.commands
     /// </summary>
     public class DayCommand : ModuleBase
     {
+        readonly (string videoLink, string title)[] dayEvent = new[] {
+            (Environment.CurrentDirectory + @"\videos\sunday.mov", "It's Energy Sword Sunday!"),
+            (Environment.CurrentDirectory + @"\videos\monado.mp4", "It is currently Monado Monday my dude!"),
+            (Environment.CurrentDirectory + @"\img\towa.png", "It's Towa Tuesday!"),
+            (Environment.CurrentDirectory + @"\videos\fish_gaming.mov", "It's Fish Gaming Wednesday fellow youths!"),
+            ("","It's Out of Touch Thursday!  https://www.youtube.com/watch?v=IneQHuAW3q8"),
+            (Environment.CurrentDirectory + @"\img\felicia_img\felicia_stare.gif", "It's Felicia Friday!"),
+            (Environment.CurrentDirectory + @"\img\radical.png", "You have entered Radical Saturday"),
+            ("","If you're seeing this message that means we've fallen out of the timeline")
+        };
+
         /// <summary>
         /// Posts a meme relative to the current day or time
         /// </summary>
-        /// <returns>Task containing the response from the day command</returns>
         [Command("day", RunMode = RunMode.Async)]
         [Alias("today")]
         [Summary("Posts a meme related to the current day or time. [Usage] !day, !today")]
@@ -19,33 +29,14 @@ namespace Feliciabot.net._6._0.commands
             var timeUtc = DateTime.UtcNow;
             TimeZoneInfo estZone = TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time");
             DayOfWeek currentDay = TimeZoneInfo.ConvertTimeFromUtc(timeUtc, estZone).DayOfWeek;
-            switch (currentDay.ToString())
+
+            if (dayEvent[(int)currentDay].videoLink == string.Empty)
             {
-                case "Monday":
-                    await Context.Channel.SendFileAsync(Environment.CurrentDirectory + @"\videos\monado.mp4", "It is currently Monado Monday my dude!");
-                    break;
-                case "Tuesday":
-                    await Context.Channel.SendFileAsync(Environment.CurrentDirectory + @"\img\towa.png", "It's Towa Tuesday!");
-                    break;
-                case "Wednesday":
-                    await Context.Channel.SendFileAsync(Environment.CurrentDirectory + @"\videos\fish_gaming.mov", "It's Fish Gaming Wednesday fellow youths!");
-                    break;
-                case "Thursday":
-                    await Context.Channel.SendMessageAsync("It's Out of Touch Thursday!  https://www.youtube.com/watch?v=IneQHuAW3q8");
-                    break;
-                case "Friday":
-                    await Context.Channel.SendFileAsync(Environment.CurrentDirectory + @"\img\felicia_img\felicia_stare.gif", "It's Felicia Friday!");
-                    break;
-                case "Saturday":
-                    await Context.Channel.SendFileAsync(Environment.CurrentDirectory + @"\img\radical.png", "You have entered Radical Saturday");
-                    break;
-                case "Sunday":
-                    await Context.Channel.SendFileAsync(Environment.CurrentDirectory + @"\videos\sunday.mov", "It's Energy Sword Sunday!");
-                    break;
-                default:
-                    await Context.Channel.SendMessageAsync($"If you're seeing this message that means we've fallen out of the timeline");
-                    break;
+                await Context.Channel.SendMessageAsync(dayEvent[(int)currentDay].title);
+                return;
             }
+
+            await Context.Channel.SendFileAsync(dayEvent[(int)currentDay].videoLink, dayEvent[(int)currentDay].title);
         }
     }
 }
