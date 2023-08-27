77f56c623ff3c9bf9e85497d6c07e9e6fd8c1da7
hamdong <mrzombielover@gmail.com>
2023-08-27 16:51:59 -0400

    Refactored ok command

--- Feliciabot.net.6.0/commands/fun/OkayCommand.cs
+++ Feliciabot.net.6.0/commands/fun/OkayCommand.cs
@@ -4,24 +4,20 @@ using Discord.Commands;
 namespace Feliciabot.net._6._0.commands
 {
     /// <summary>
-    /// Commands pertaining to calling out ok
+    /// Commands pertaining to ok
     /// </summary>
     public class OkayCommand : ModuleBase
     {
         /// <summary>
         /// Posts ok with the previous member's username or a specified username
         /// </summary>
-        /// <returns>Nothing, just posts ok</returns>
         [Command("ok", RunMode = RunMode.Async)]
         [Summary("Posts ok [previous member username]. [Usage] !ok")]
         public async Task Ok()
         {
-            // Get last batch of messages
-            IEnumerable<IMessage> messages;
-            messages = await Context.Channel.GetMessagesAsync(50).FlattenAsync();
-
-            // Get the last user who posted
+            IEnumerable<IMessage> messages = await Context.Channel.GetMessagesAsync(50).FlattenAsync();
             string callOutUser = Context.Message.Author.Username;
+
             foreach (IMessage m in messages)
             {
                 if (m.Author != Context.Message.Author)
@@ -31,7 +27,6 @@ namespace Feliciabot.net._6._0.commands
                 }
             }
 
-            // Call out
             await Context.Channel.SendMessageAsync("ok " + callOutUser);
         }
 
@@ -39,12 +34,8 @@ namespace Feliciabot.net._6._0.commands
         /// Overloaded method, posts ok with the mentioned username
         /// </summary>
         /// <param name="user">User to call out</param>
-        /// <returns>Nothing, just posts ok</returns>
         [Command("ok", RunMode = RunMode.Async)]
         [Summary("Posts ok [mentioned member username]. [Usage] !ok @member")]
-        public async Task Ok(IUser user)
-        {
-            await Context.Channel.SendMessageAsync("ok " + user.Username);
-        }
+        public async Task Ok(IUser user) => await Context.Channel.SendMessageAsync("ok " + user.Username);
     }
 }
